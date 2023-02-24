8bfa5bd324707fa0c022bf37a2ab0ed7721f3ef4
max <maxhthomas@outlook.com>
2023-02-24 23:57:45 +0000

    Manually add products

--- src/components/index/Index.js
+++ src/components/index/Index.js
@@ -1,32 +1,32 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from './Product';
-import Range from './Range';
+import ProductAdder from './ProductAdder';
 
 const Index = () => {
-  const [amount, setAmount] = useState(2)
+  const [product, setProduct] = useState()
   const [products, setProducts] = useState([])
-  useEffect(() => {
-    let arr = []
-    for(let i = 1; i <= amount; i++) {
-      arr.unshift({header: 'Header', title: 'Title', body: 'Body'})
-    }
-    setProducts(arr)
-  }, [amount])
 
+  if (product) {
+    setProducts([
+      ...products,
+      product
+    ])
+    setProduct()
+  }
 
   return (
     <Container>
         <Row style={{margin: '10px'}}>
           <Col>
-            <Range set={setAmount}/>
+            <ProductAdder product={setProduct}/>
           </Col>
         </Row>
-        <Row style={{margin: '10px', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+        <Row style={{margin: '10px', display: 'flex', justifyContent: 'center'}}>
             {((products.length > 0) ? products.map((product, index) => {
-            return <Product header={product.header+' '+(index+1)} title={product.title+' '+(index+1)} body={product.body+' '+(index+1)}/>
+            return <Product key={index} header={product.header} title={product.title} body={product.body}/>
             }) : null)}
         </Row>
     </Container>
---
+++ src/components/index/ProductAdder.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+const ProductAdder = ({ product }) => {
+  const [header, setHeader] = useState([])
+  const [title, setTitle] = useState([])
+  const [body, setBody] = useState([])
+
+  const handleSubmit = () => {
+    product({header: header, title: title, body: body})
+  }
+
+  return (
+    <Form style={{fontWeight: 'bold'}}>
+      <Form.Group className="mb-3">
+        <Form.Label>Product Header</Form.Label>
+        <Form.Control value={header} onChange={e => setHeader(e.target.value)} type="text" placeholder="Product Header" />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Product Title</Form.Label>
+        <Form.Control value={title} onChange={e => setTitle(e.target.value)} type="text" placeholder="Product Title" />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Product Body</Form.Label>
+        <Form.Control value={body} onChange={e => setBody(e.target.value)} type="text" placeholder="Product Body" />
+      </Form.Group>
+
+      <Button onClick={handleSubmit} className='btn-success'>Submit</Button>
+    </Form>
+  )
+}
+
+export default ProductAdder
